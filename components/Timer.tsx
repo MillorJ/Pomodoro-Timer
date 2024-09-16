86a2c9196fc96d7ab1be3bc5145af45f9649c5e4
John Rey Millor <johnreymillor69@gmail.com>
2024-09-16 23:53:40 +0800

    Update Timer.tsx

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -52,3 +52,10 @@ const Timer: React.FC<TimerProps> = ({ title, minutes, onComplete }) => {
           className="px-4 py-2 bg-yellow-500 hover:bg-yellow-700 text-white rounded-full mx-2"
           onClick={() => { setIsPaused(!isPaused); }}
         ></button>
+
+{isPaused ? <FaPlay /> : <FaPause />}
+        </button>
+        <button
+          className="px-4 py-2 bg-red-500 hover:bg-red-700 text-white rounded-full mx-2"
+          onClick={() => { setIsActive(false); setTime(minutes * 60); }}
+        >
