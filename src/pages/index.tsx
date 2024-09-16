06fbc4431eca3c3fdd04a804a4418c5cf9242590
John Rey Millor <johnreymillor69@gmail.com>
2024-09-16 23:44:43 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,3 +2,4 @@ import { useState } from 'react';
 import Timer from '../../components/Timer';
 
 const Home = () => {
+  const [activeTimer, setActiveTimer] = useState<'pomodoro' | 'short' | 'long' | null>(null);
