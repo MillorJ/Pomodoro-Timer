2d92119be3689a315536be48cae199477ba7529d
John Rey Millor <johnreymillor69@gmail.com>
2024-09-16 23:45:43 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,4 +9,9 @@ const Home = () => {
     const startPomodoro = () => {
       setActiveTimer('pomodoro');
     };
+
+    const startShortBreak = () => {
+      setActiveTimer('short');
+    };
+  
   
