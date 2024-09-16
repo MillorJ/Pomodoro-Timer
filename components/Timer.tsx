edbfa0f71fd1ce9150add766b11f60dffcebd202
John Rey Millor <johnreymillor69@gmail.com>
2024-09-16 23:52:57 +0800

    Update Timer.tsx

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,2 +1,13 @@
 import { useState, useEffect } from 'react';
 import { FaPlay, FaPause, FaStop } from 'react-icons/fa';
+
+interface TimerProps {
+  title: string;
+  minutes: number;
+  onComplete: () => void;
+}
+
+const Timer: React.FC<TimerProps> = ({ title, minutes, onComplete }) => {
+  const [time, setTime] = useState(minutes * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
