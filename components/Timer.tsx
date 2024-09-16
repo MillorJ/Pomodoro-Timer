8b9fd99f3c7cc89f8a78b482f3353f175fcfecb3
John Rey Millor <johnreymillor69@gmail.com>
2024-09-16 23:53:08 +0800

    Update Timer.tsx

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -11,3 +11,19 @@ const Timer: React.FC<TimerProps> = ({ title, minutes, onComplete }) => {
   const [time, setTime] = useState(minutes * 60);
   const [isActive, setIsActive] = useState(false);
   const [isPaused, setIsPaused] = useState(true);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isActive && !isPaused) {
+      timer = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime === 1) {
+            onComplete();
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+    } else if (isPaused || !isActive) {
+      clearInterval(timer);
+    }
